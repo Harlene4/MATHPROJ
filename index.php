e6fa54685f2ea73b7d7475e1caddfa65e17f4205
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-25 14:10:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 <body>
 <center>
 <body bgcolor= "beige">
-<audio src= "file:///C:/Users/user/Desktop/SAMPLE%20(1)/BTS%20(%EB%B0%A9%ED%83%84%EC%86%8C%EB%85%84%EB%8B%A8)%20-%200000%20(Zero%20O'Clock)%20(Color%20Coded%20Lyrics%20EngRomHan%EA%B0%80%EC%82%AC).mp3"  controls </audio>
+<a href="file:///C:/Users/user/Desktop/SAMPLE%20(1)/BTS%20(%EB%B0%A9%ED%83%84%EC%86%8C%EB%85%84%EB%8B%A8)%20-%200000%20(Zero%20O'Clock)%20(Color%20Coded%20Lyrics%20EngRomHan%EA%B0%80%EC%82%AC).mp3">file:///C:/Users/user/Desktop/SAMPLE%20(1)/BTS%20(%EB%B0%A9%ED%83%84%EC%86%8C%EB%85%84%EB%8B%A8)%20-%200000%20(Zero%20O'Clock)%20(Color%20Coded%20Lyrics%20EngRomHan%EA%B0%80%EC%82%AC).mp3</a>
+controls </a href>
 </font> 
 </center>
 <p> 
